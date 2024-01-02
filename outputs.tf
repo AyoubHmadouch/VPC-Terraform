a20bb89eac101fd7f36e577ed8dec4ff22257ffd
Ayoub.Hmadouch <Ayoubhmd@hotmail.fr>
2024-01-02 14:08:13 +0100

    Add other outputs in outputs.tf.

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,23 @@ output "private_subnets_ids" {
   value       = ["${aws_subnet.private_subnets[*].id}"]
   description = "The private subntes ids."
 }
+
+output "public_route_table_id" {
+  value       = aws_route_table.public_route_table.id
+  description = "The id of the public route table."
+}
+
+output "private_route_table_id" {
+  value       = aws_route_table.private_route_table.id
+  description = "The id of the private route table."
+}
+
+output "ngw_id" {
+  value       = var.create_ngw ? aws_nat_gateway.ngw[0].id : null
+  description = "The id of the nat gateway."
+}
+
+output "nat_eip" {
+  value       = var.create_ngw ? aws_eip.ngw_eip[0].public_ip : null
+  description = "The id of the nat gateway."
+}
